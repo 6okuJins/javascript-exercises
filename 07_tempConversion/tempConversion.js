9d78b014ea9e3124b9b3ba5551fcd79b49ea73b5
6okuJins <d.ma@mail.utoronto.ca>
2022-03-31 14:12:37 -0400

    Complete seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let result = Math.round((temperature - 32) * (5/9) * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let result = Math.round((temperature * (9/5) + 32) * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
