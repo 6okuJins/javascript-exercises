ddf7fac98daff9dd21e4da5bb190d4ea25b825cf
6okuJins <d.ma@mail.utoronto.ca>
2022-03-31 12:07:48 -0400

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(String) {
+    let result = "";
+    for (let i = String.length - 1; i >= 0; i--) {
+        result += String[i]
+    }
+    return result;
 };
 
 // Do not edit below this line
