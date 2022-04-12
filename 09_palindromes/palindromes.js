fa55030fedc06c37342b229a1bc26cf815a6fe4a
6okuJins <d.ma@mail.utoronto.ca>
2022-04-12 12:45:39 -0400

    Complete ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let processed = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (processed
+        .split("")
+        .reverse()
+        .join("")
+        == processed)
 };
 
 // Do not edit below this line
