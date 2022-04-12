e7b772688135d71e1e4f33772aa11e8446d1e292
6okuJins <d.ma@mail.utoronto.ca>
2022-04-12 14:30:54 -0400

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (typeof(n) == 'string') n = parseInt(n);
+    let array = Array(n).fill(1).reduce((result,_,i) => {
+        result.push((i <= 1) ? 1 : result[i-2] + result[i-1]);
+        return result;
+    }, [])
+    return array.pop()
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
