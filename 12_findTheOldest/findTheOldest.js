31deb061226d5bc4b66ce128925cb21e9e343980
6okuJins <d.ma@mail.utoronto.ca>
2022-04-12 16:54:02 -0400

    Complete twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const year = (new Date()).getFullYear();
+    const oldest = people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = ("yearOfDeath" in oldestPerson) ?
+                        oldestPerson.yearOfDeath - oldestPerson.yearOfBirth :
+                        year - oldestPerson.yearOfBirth;
+        const currentAge = ("yearOfDeath" in currentPerson) ?
+                        currentPerson.yearOfDeath - currentPerson.yearOfBirth:
+                        year - currentPerson.yearOfBirth;
+        return ((oldestAge > currentAge) ? oldestPerson : currentPerson);
+    });
+    return oldest;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
